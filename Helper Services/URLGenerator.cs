ff7e8755cda085c4090a0496bd7f639493c72cb9
Kiril Chuchev <54711535+KirilChuchev@users.noreply.github.com>
2021-08-10 15:49:21 +0300

    Implement HtmlSample sharable URL generator

---
+++ Helper Services/URLGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace HTMLPreviewerApp.Helper_Services
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Linq;
+
+    public static class URLGenerator
+    {
+        public static string Generate(HttpRequest request, string sharableActionName, string htmlSampleId)
+        {
+            var scheme = request.HttpContext.Request.Scheme;
+            var hostName = request.Host.Value;
+            var controllerName = request.RouteValues.Select(x => x.Value).ToArray()[1];
+
+            return $"{scheme}://{hostName}/{controllerName}/{sharableActionName}/{htmlSampleId}";
+        }
+    }
+}
